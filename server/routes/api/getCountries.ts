b4e0596246ed624ec62cfaa8ee6e03f92111522e
Gustavo Ferrara <73325370+gustavoferrara@users.noreply.github.com>
2024-09-08 22:57:21 -0300

    add country population to info endpoint

--- server/routes/api/getCountries.ts
+++ server/routes/api/getCountries.ts
@@ -2,13 +2,13 @@ import 'dotenv-safe/config';
 
 import express, { Request, Response } from 'express';
 
-import axiosFetch from '../../axiosFetch';
+import fetchJson from '../../helpers/fetcher';
 
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
   try {
-    const availableCountries = await axiosFetch(
+    const availableCountries = await fetchJson(
       'https://date.nager.at/api/v3/AvailableCountries',
     );
 
@@ -19,15 +19,29 @@ router.get('/', async (req: Request, res: Response) => {
   }
 });
 
-router.get('/info/:country', async (req: Request, res: Response) => {
+router.get('/info/:countryCode', async (req: Request, res: Response) => {
   try {
-    const { country } = req.params;
+    const { countryCode } = req.params;
 
-    const countryInfo = await axiosFetch(
-      `https://date.nager.at/api/v3/CountryInfo/${country}`,
+    const countryInfo = await fetchJson(
+      `https://date.nager.at/api/v3/CountryInfo/${countryCode}`,
     );
 
-    res.status(200).json({ countryInfo });
+    const borderCountries = countryInfo.borders;
+    const countryName = countryInfo.commonName.toLowerCase();
+
+    const countryPopulation = await fetchJson(
+      'https://countriesnow.space/api/v0.1/countries/population',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ country: countryName }),
+      },
+    );
+
+    res.status(200).json({ borderCountries, countryPopulation });
   } catch (err: any) {
     console.error(err);
     res.status(500).json({ error: 'Server error' });
