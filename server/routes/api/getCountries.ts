4ea5cc2bb62bd81f4b4234ff90fb60f49df096b3
Gustavo Ferrara <73325370+gustavoferrara@users.noreply.github.com>
2024-09-08 22:44:19 -0300

    add countries info endpoint

--- server/routes/api/getCountries.ts
+++ server/routes/api/getCountries.ts
@@ -19,4 +19,19 @@ router.get('/', async (req: Request, res: Response) => {
   }
 });
 
+router.get('/info/:country', async (req: Request, res: Response) => {
+  try {
+    const { country } = req.params;
+
+    const countryInfo = await axiosFetch(
+      `https://date.nager.at/api/v3/CountryInfo/${country}`,
+    );
+
+    res.status(200).json({ countryInfo });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
 export default router;
