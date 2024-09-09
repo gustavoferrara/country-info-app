a7a07559f6753ad0e72f72fb4286f6c17a27e682
Gustavo Ferrara <73325370+gustavoferrara@users.noreply.github.com>
2024-09-08 23:59:30 -0300

    call countries list endpoint on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,43 @@
-const Home: React.FC = () => {
+import { GetServerSideProps } from 'next';
+import { useEffect } from 'react';
+
+import axiosFetch from '@/helpers/axiosFetch';
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const countryList = await axiosFetch(
+    'http://localhost:3000/api/getCountries/',
+  );
+
+  return {
+    props: { countryList },
+    // revalidate: 60,
+  };
+};
+
+export interface BorderCountry {
+  commonName: string;
+  officialName: string;
+  countryCode: string;
+  region: string;
+  borders: any;
+}
+
+export interface CountryPopulation {
+  year: number;
+  value: number;
+}
+
+interface HomeProps {
+  countryList: {
+    availableCountries: Array<{ countryCode: string; name: string }>;
+  };
+}
+
+const Home: React.FC<HomeProps> = ({ countryList }) => {
+  useEffect(() => {
+    console.log(countryList);
+  }, []);
+
   return (
     <>
       <div>Hello world!</div>
