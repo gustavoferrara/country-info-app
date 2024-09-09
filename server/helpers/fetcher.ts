cf86dde2629f6d44f4a0096c6b8240a3c1db3638
Gustavo Ferrara <73325370+gustavoferrara@users.noreply.github.com>
2024-09-09 01:12:12 -0300

    simplify helper fetch function

--- server/helpers/fetcher.ts
+++ server/helpers/fetcher.ts
@@ -1,20 +1,11 @@
 const fetchJson = async (url: string, options?: RequestInit) => {
-  try {
-    if (!options) options = { method: 'GET' };
+  if (!options) options = { method: 'GET' };
 
-    const response = await fetch(url, options);
+  const response = await fetch(url, options);
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
+  const data = await response.json();
 
-    const data = await response.json();
-
-    return data;
-  } catch (error) {
-    console.error('Fetch error:', error);
-    throw error;
-  }
+  return data;
 };
 
 export default fetchJson;
